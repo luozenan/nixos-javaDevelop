13cf1013a9d28e674bde168108a2ad7476fda5dc
luozenan <162779246+luozenan@users.noreply.github.com>
2026-03-07 17:09:06 +0800

    Remove wechat override and add wechat-uos

--- modules/app.nix
+++ modules/app.nix
@@ -12,12 +12,7 @@
      # nix store add-file 
      # wechat
      qq
-     (pkgs.wechat.overrideAttrs (oldAttrs: {
-        src = fetchurl {
-          url = "https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.AppImage";
-          hash = "sha256-+r5Ebu40GVGG2m2lmCFQ/JkiDsN/u7XEtnLrB98602w=";
-        };
-     }))
+     wechat-uos
      thunderbird
      # 远程
      parsec-bin
